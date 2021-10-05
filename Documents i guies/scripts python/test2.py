2120897536cd256effc92d391876d14b7d683a17
Federico Adria <fadriacarrasco@gmail.com>
2021-10-05 22:55:59 +0200

    Scripts i guia d'instalació

---
+++ Documents i guies/scripts python/test2.py
@@ -0,0 +1,5 @@
+import pyjokes
+  
+My_joke = pyjokes.get_joke(language="es", category="neutral")
+  
+print(My_joke)
---
+++ Documents i guies/scripts python/test3.py
@@ -0,0 +1,5 @@
+import pyjokes
+  
+My_joke = pyjokes.get_joke(language="en", category="chuck")
+  
+print(My_joke)
